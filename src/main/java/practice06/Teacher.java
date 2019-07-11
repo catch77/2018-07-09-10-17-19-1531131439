0d2ed7051074d082a721d4dfe3350cdb983fdfbf
catch77 <1297056296@qq.com>
2019-07-11 19:23:56 +0800

    finish practise 5&6&7

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,34 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass = -1;
+
+    public Teacher(int klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass != -1)
+            return super.introduce()+" I am a Teacher. I teach Class "+klass+".";
+        else
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+    }
 }
