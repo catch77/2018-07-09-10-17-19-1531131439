32e19d7fba622e368b8cf3e7078027045ca01e88
catch77 <1297056296@qq.com>
2019-07-11 18:30:16 +0800

    finish practise 3

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,28 @@
 package practice03;
 
-public class Student {
+import practice03.Person;
+
+public class Student extends Person {
+    private int klass;
+
+    public Student() {
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class "+klass+".";
+    }
 }
